4e4ad34f5bc9c267c8b302ef8ad4f4c44b7c47a0
Trevor Wilson <tbwilson@mabankisd.net>
2017-05-30 16:17:50 -0500

    minor changes

--- src/components/rowsection.js
+++ src/components/rowsection.js
@@ -4,9 +4,9 @@ export default class RowSection extends Component {
   render(){
     if(this.props.currentValue.controllingPlayer !== 1 && this.props.currentValue.controllingPlayer !== 2){
       let tileStyle = this.props.currentValue.selected === true ? 'selectedTile' : this.props.currentValue.inRange === true ? 'tileInRange' : ''
-      let controlClass = this.props.currentValue.controllingPlayer === 1 ? 'playerOneControls' : 'playerTwoControls'
+      let controlClass = this.props.currentValue.controllingPlayer === 1 ? 'playerOneControls' : this.props.currentValue.controllingPlayer === 2 ? 'playerTwoControls' : ''
       return(
-        <div id={'row'+this.props.curRow+'col'+this.props.curCol} className={'boardSection '+ tileStyle + controlClass} onClick={() => this.props.handleClick(this.props.curRow, this.props.curCol)}>
+        <div id={'row'+this.props.curRow+'col'+this.props.curCol} className={'boardSection '+ tileStyle +' '+ controlClass} onClick={() => this.props.handleClick(this.props.curRow, this.props.curCol)}>
 
         </div>
       )
